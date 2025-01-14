a7b99f49510f32896fc3a4cd18b663c80ae99af7
avinash <avinash21961@gmail.com>
2025-01-14 14:04:19 -0500

    with SUite non working code

--- src/test/java/com/demo/SpringBootCucumberDemo/CucumberTest.java
+++ src/test/java/com/demo/SpringBootCucumberDemo/CucumberTest.java
@@ -1,16 +1,18 @@
 package com.demo.SpringBootCucumberDemo;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "com.demo.SpringBootCucumberDemo",
-        plugin = {"pretty", "html:target/cucumber-reports.html"}
-)
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+
+@Suite
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.demo.SpringBootCucumberDemo.steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberTest {
 }
