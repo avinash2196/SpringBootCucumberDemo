3a425619eac51e61eb5700f661890b54122388ab
avinash <avinash21961@gmail.com>
2025-01-14 14:31:49 -0500

    with SUite non working code

--- src/test/java/com/demo/SpringBootCucumberDemo/CucumberTest.java
+++ src/test/java/com/demo/SpringBootCucumberDemo/CucumberTest.java
@@ -1,18 +1,20 @@
 package com.demo.SpringBootCucumberDemo;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 
 @Suite
+@IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.demo.SpringBootCucumberDemo.steps")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.serenityReporterParallel,pretty,timeline:target/test-results/timeline")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberTest {
 }
