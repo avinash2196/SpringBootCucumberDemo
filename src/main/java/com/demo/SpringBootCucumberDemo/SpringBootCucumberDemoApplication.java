906446b820167ffe984f8dbe3940bd98dc9ff7a8
avinash <avinash21961@gmail.com>
2025-01-13 22:27:11 -0500

    updated with running code with service layer

--- src/main/java/com/demo/SpringBootCucumberDemo/SpringBootCucumberDemoApplication.java
+++ src/main/java/com/demo/SpringBootCucumberDemo/SpringBootCucumberDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootCucumberDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCucumberDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCucumberDemoApplication.class, args);
+    }
 }
